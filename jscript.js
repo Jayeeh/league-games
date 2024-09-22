34b38e3f3b0cfc211e877490489366311f33fd50
Jayeeh <erroljustinecasbadillo23@gmail.com>
2024-09-22 22:00:43 +0800

    Initialize League Games - Just a very simple code

---
+++ jscript.js
@@ -0,0 +1,18 @@
+// Define an object 'player' with properties
+const player = {
+    playerName: "Leo", // Name of the player
+    goalsScored: 4, // Number of goals scored by the player
+    gamesPlayed: 2 // Number of games played by the player
+  };
+  
+  // Define an object 'team' with properties
+  const team = {
+    teamName: "Lions", // Name of the team
+    numberPlayers: 18, // Total number of players in the team
+    varsity: true, // Indicates if the team is a varsity team
+    member: player // Reference to the 'player' object
+  };
+  
+  // Log the 'team' object to the console
+  console.log(team);
+  
